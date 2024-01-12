84d2868d4c230aad81fb10cb70bcbce444f99fbe
guimoouraa <guilherme.mouraa@outlook.com>
2024-01-12 18:33:19 -0300

    created registration form

--- public/unidade_08/inserir.php
+++ public/unidade_08/inserir.php
@@ -8,6 +8,7 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/crud.css" rel="stylesheet">
     </head>
 
     <body>
@@ -15,7 +16,20 @@
         <?php include_once("../_incluir/funcoes.php"); ?> 
         
         <main>  
-            
+            <div id="janela_formulario">
+                <form action="inserir.php" method="post">
+                    <input type="text" name="nometransportadora" placeholder="Nome">
+                    <input type="text" name="endereco" placeholder="Endereço">
+                    <input type="text" name="cidade" placeholder="Cidade">
+                    <select name="estador">
+                        <option value="1">Acre</option>
+                    </select>
+                    <input type="text" name="telefone" placeholder="Telefone">
+                    <input type="text" name="cep" placeholder="CEP">
+                    <input type="text" name="cnpj" placeholder="CNPJ">
+                    <input type="submit" value="inserir">
+                </form>
+            </div>
         </main>
 
         <?php include_once("../_incluir/rodape.php"); ?>  
