f8993d1f7c959c3677a21611b0fcf8ac084e6dda
guimoouraa <guilherme.mouraa@outlook.com>
2024-01-13 23:17:12 -0300

    Conclued page insert values in sistem

--- public/unidade_08/inserir.php
+++ public/unidade_08/inserir.php
@@ -1,5 +1,35 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 
+<?php 
+    // inserção no banco
+    if (isset( $_POST["cidade"] )) {
+        $nome     = $_POST["nometransportadora"];
+        $endereco = $_POST["endereco"];
+        $cidade   = $_POST["cidade"];
+        $estado  = $_POST["estado"];
+        $telefone = $_POST["telefone"];
+        $cep      = $_POST["cep"];
+        $cnpj     = $_POST["cnpj"];
+
+
+        $inserir = "INSERT INTO transportadoras (nometransportadora, endereco, telefone, cidade, estadoID, cep, cnpj) VALUES ('$nome','$endereco','$telefone','$cidade',$estado,'$cep','$cnpj')";
+        $operacao_inserir = mysqli_query($conecta,$inserir);
+        if (!$operacao_inserir) {
+            die("Falha na inserção");
+        } else {
+            header("location:listagem.php");
+        }
+    }
+
+    //Seleção de estador 
+    $estados = "SELECT nome, estadoID FROM estados";
+    $linha_estados = mysqli_query($conecta,$estados);
+    if ( !$linha_estados ) {
+        die("erro no banco");
+    }
+
+?>
+
 <!doctype html>
 <html>
     <head>
@@ -21,13 +51,17 @@
                     <input type="text" name="nometransportadora" placeholder="Nome">
                     <input type="text" name="endereco" placeholder="Endereço">
                     <input type="text" name="cidade" placeholder="Cidade">
-                    <select name="estador">
-                        <option value="1">Acre</option>
+                    <select name="estado">
+                        <?php while($linha = mysqli_fetch_assoc($linha_estados)) { ?>
+                        <option value="<?php echo $linha["estadoID"]; ?>">
+                            <?php echo $linha["nome"]; ?>
+                        </option>
+                        <?php } ?>
                     </select>
                     <input type="text" name="telefone" placeholder="Telefone">
                     <input type="text" name="cep" placeholder="CEP">
                     <input type="text" name="cnpj" placeholder="CNPJ">
-                    <input type="submit" value="inserir">
+                    <input type="submit" value="Inserir">
                 </form>
             </div>
         </main>
