f15e63acd16d0a217cc2e4758f4f961d70233dea
guimoouraa <guilherme.mouraa@outlook.com>
2024-01-16 23:49:08 -0300

    the form alter data

--- public/unidade_09/alteracao.php
+++ public/unidade_09/alteracao.php
@@ -15,7 +15,36 @@
         <?php include_once("../_incluir/funcoes.php"); ?> 
         
         <main>  
-            
+           <div id="janela_formulario">
+            <form action="alteracao.php" method="post">
+                <h2>Alteração de transportadora</h2>
+
+                <label for="nometransportadora">Nome da transportadora</label>
+                <input type="text" value="" name="nometransportadora">
+
+                <label for="endereco">Endereço</label>
+                <input type="text" value="" name="endereco">
+
+                <label for="cidade">Cidade</label>
+                <input type="text" value="" name="cidade">
+
+                <label for="estados">Estado</label>
+                <input type="text" value="" name="estado">
+
+                <label for="cep">CEP</label>
+                <input type="text" value="" name="cep">
+
+                <label for="telefone">Telefone</label>
+                <input type="text" value="" name="telefone">
+
+                <label for="cnpj">CNPJ</label>
+                <input type="text" value="" name="cnpj">
+
+                <input type="hidden">
+
+                <input type="submit">
+            </form>
+           </div> 
         </main>
 
         <?php include_once("../_incluir/rodape.php"); ?>  
