fdf1da355f9ec4183e4f9dea12a48d85938ac846
guimoouraa <guilherme.mouraa@outlook.com>
2024-01-23 15:29:58 -0300

    create query's transportadora and estados

--- public/unidade_09/alteracao.php
+++ public/unidade_09/alteracao.php
@@ -1,5 +1,52 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 
+<?php 
+    if (isset($_POST["cidade"])) {
+        $nome     = $_POST["nometransportadora"];
+        $endereco = $_POST["endereco"];
+        $cidade   = $_POST["cidade"];
+        $estado   = $_POST["estados"];
+        $cep      = $_POST["cep"];
+        $cnpj     = $_POST["cnpj"];
+        $telefone = $_POST["telefone"];
+        $tid      = $_POST["transportadoraID"];
+
+
+        
+    }
+
+
+
+    // Consulta na tabela transportadora
+    $transp = "SELECT *  FROM transportadoras ";
+
+    if ( isset($_GET["codigo"]) )
+    {
+        $id =  $_GET["codigo"];
+        $transp .= "WHERE transportadoraID = {$id}";
+    } else {
+        $transp .= "WHERE transportadoraID = 1";
+    }
+
+
+    $con_transp = mysqli_query($conecta,$transp);
+    if(!$con_transp) {
+        die("Falha no banco de dados");
+    } else {
+        $info_transp = mysqli_fetch_assoc($con_transp);
+    }
+
+    // consulta na tabela de estados
+
+    $estados = "SELECT estadoID, nome FROM estados";
+    $lista_estados = mysqli_query($conecta,$estados);
+
+    if(!$lista_estados) {
+        die("Falha no banco ");
+    }
+
+?>
+
 <!doctype html>
 <html>
     <head>
@@ -8,6 +55,7 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/alteracao.css" rel="stylesheet">
     </head>
 
     <body>
@@ -20,29 +68,49 @@
                 <h2>Alteração de transportadora</h2>
 
                 <label for="nometransportadora">Nome da transportadora</label>
-                <input type="text" value="" name="nometransportadora">
+                <input type="text" value="<?php echo $info_transp["nometransportadora"] ?> " name="nometransportadora">
 
                 <label for="endereco">Endereço</label>
-                <input type="text" value="" name="endereco">
+                <input type="text" value="<?php echo $info_transp["endereco"] ?> " name="endereco">
 
                 <label for="cidade">Cidade</label>
-                <input type="text" value="" name="cidade">
+                <input type="text" value="<?php echo $info_transp["cidade"] ?> " name="cidade">
 
-                <label for="estados">Estado</label>
-                <input type="text" value="" name="estado">
+                <label for="estados">Estados</label>
+                <select id="estados" value="" name="estado">
+                    <?php
+                        $meuestado = $info_transp["estadoID"];
+                        while($linha = mysqli_fetch_assoc($lista_estados)) {
+                            $estado_momento = $linha["estadoID"];
+                            if ($meuestado == $estado_momento) {
+                    ?>
+                    <option value="<?php echo $info_transp["estadoID"]; ?>" selected >
+                        <?php echo $linha["nome"];?>
+                    </option>
+                    <?php
+                        } else {
+                    ?>
+                    <option value="<?php echo $info_transp["estadoID"]; ?>">
+                        <?php echo $linha["nome"];?>
+                    </option>
+                    <?php
+                        }
+                       } 
+                    ?>
+                </select>
 
                 <label for="cep">CEP</label>
-                <input type="text" value="" name="cep">
+                <input type="text" value="<?php echo $info_transp["cep"] ?> " name="cep">
 
                 <label for="telefone">Telefone</label>
-                <input type="text" value="" name="telefone">
+                <input type="text" value="<?php echo $info_transp["telefone"] ?> " name="telefone">
 
                 <label for="cnpj">CNPJ</label>
-                <input type="text" value="" name="cnpj">
+                <input type="text" value="<?php echo $info_transp["cnpj"] ?> " name="cnpj">
 
-                <input type="hidden">
+                <input type="hidden" name="transportadoraID" value="<?php echo $info_transp["transportadoraID"] ?> ">
 
-                <input type="submit">
+                <input type="submit" value="Confirmar Alteração">
             </form>
            </div> 
         </main>
