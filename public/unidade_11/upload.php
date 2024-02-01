ee49e69ec44b0becfe92fdb164113d64f8a94502
guimoouraa <guilherme.mouraa@outlook.com>
2024-02-01 17:21:32 -0300

    added function upload file

--- public/unidade_11/upload.php
+++ public/unidade_11/upload.php
@@ -1,5 +1,19 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 
+<?php 
+    if( isset($_POST["enviar"])) {
+      $pasta_temporaria = $_FILES["upload_file"]["tmp_name"];
+      $arquivo          = $_FILES["upload_file"]["name"];
+      $pasta            = "uploads";  
+
+     if (move_uploaded_file($pasta_temporaria, $pasta . "/" . $arquivo)) {
+        $mensagem = "Arquivo publicado com sucesso.";
+     } else {
+        $mensagem = "Erro na publicação";
+     }
+    }
+?>
+
 <!doctype html>
 <html>
     <head>
@@ -22,11 +36,17 @@
         <?php include_once("../_incluir/funcoes.php"); ?>  
         
             <main>  
-                <div>
-                    <form action="">
-                        <input type="text">
-                        <input type="submit">
+                <div id="janela_formulario">
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input type="file" name="upload_file">
+                        <input type="submit" name="enviar">
                     </form>
+                    <?php 
+                        if ( isset($mensagem)){
+                            echo $mensagem;
+                        }
+                    
+                    ?>
                 </div>
             </main>
 
